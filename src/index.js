29c385f2c5091d8fca1a237127cb224e020ffcec
bhanu <bhanukumar@nichesolv.com>
2023-04-13 21:57:44 +0530

    intial

---
+++ src/index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const axios = require("axios");
+const userService = async (url = "", payload = "", token = "") => {
+  url = url || process.env.BASE_URL;
+  try {
+    await axios.get(url, payload);
+    return { url, payload, token };
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+module.exports = { userService };
